16b41eb3aa5f8deb908978f46c33b6eec163129e
Laurent KHOUDJA <Laurentkh@gmail.com>
2013-10-10 13:48:28 +0200

    Modification des alert flash : icones modifiés

--- core/Alerts.php
+++ core/Alerts.php
@@ -8,12 +8,26 @@ class Alerts{
   }
 
 	public function alert(){
+    switch ($this->alert['type']){
+      case 'success':
+        $icone = 'thumbs-up';
+        break;
+      case 'success': 
+        $icone = 'info-sign';
+        break;
+      case 'danger': 
+        $icone = 'remove-sign';
+        break;
+      default:
+         $icone = 'info-sign';
+        break;
+  }
     ?>
   <hr/>  
   <div class="container">
 	 <div class="bs-example row">
       <div class="panel panel-<?=$this->alert['type']?>">
-          <div class="panel-heading"><h3><span class="glyphicon glyphicon-info-sign"></span> <?=$this->alert['title']?></h3></div>
+          <div class="panel-heading"><h3><span class="glyphicon glyphicon-<?=$icone?>"></span> <?=$this->alert['title']?></h3></div>
           <div class="panel-body">
             <p><?=$this->alert['message']?></p>
             <?php if(isset($this->alert['list'])): ?>
