0d890cbc4f928eedd6b8f032a2a908c1352a87fd
Laurent KHOUDJA <Laurentkh@gmail.com>
2013-11-20 16:56:59 +0100

    CSS/JS local
    passage des css et js en local

--- view/layout/default.php
+++ view/layout/default.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8" />
     <title>PLAN Qrcode - scanne et assure ...</title>
 
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?=BASE_URL.DS.'css'.DS?>bootstrap.min.css">
     <!-- Optional theme -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="<?=BASE_URL.DS.'css'.DS?>bootstrap-theme.min.css">
         <link rel="stylesheet" href="<?=BASE_URL.DS.'css'.DS?>style.css">
 </head>
  <body>
@@ -78,7 +78,7 @@
         </div>
       </footer>
     </div> <!-- /container -->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script src="<?=BASE_URL.DS.'js'.DS?>jquery.min.js"></script>
+    <script src="<?=BASE_URL.DS.'js'.DS?>bootstrap.min.js"></script>
   </body>
 </html>
